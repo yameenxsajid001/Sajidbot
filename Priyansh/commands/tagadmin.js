fc0fca96320d5563c4a1b36bbfca7b9a346d8573
MR. AMIR <136250878+amirfarebii34310@users.noreply.github.com>
2025-05-29 18:07:15 +0500

    Create tagadmin.js

---
+++ Priyansh/commands/tagadmin.js
@@ -0,0 +1,112 @@
+const fs = require('fs');
+const moment = require('moment-timezone');
+module.exports.config = {
+    name: "tagadmin", // Tên lệnh, được sử dụng trong việc gọi lệnh
+    version: "1.0.0", // phiên bản của module này
+    hasPermssion: 1, // Quyền hạn sử dụng, với 0 là toàn bộ thành viên, 1 là quản trị viên trở lên, 2 là admin/owner
+    credits: "ATF-TEAM", // TruongMini
+    description: "Tag!!", // Thông tin chi tiết về lệnh
+    commandCategory: "System", // Thuộc vào nhóm nào: system, other, game-sp, game-mp, random-img, edit-img, media, economy, ...
+    usages: "[msg]", // Cách sử dụng lệnh
+    cooldowns: 5 // Thời gian một người có thể lặp lại lệnh
+};
+module.exports.onLoad = () => {
+    const fs = require("fs-extra");
+    const request = require("request");
+    const dirMaterial = __dirname + `/noprefix/`;
+    if (!fs.existsSync(dirMaterial + "noprefix")) fs.mkdirSync(dirMaterial, { recursive: true });
+    if (!fs.existsSync(dirMaterial + "bank.gif")) request("https://i.imgur.com/shwqrHd.gif ").pipe(fs.createWriteStream(dirMaterial + "bank.gif"));
+                       }
+
+module.exports.handleReply = async function ({ api, event, handleReply, Users, Threads, args }) {
+let uid = event.senderID;
+var msg = [`ljkj`];
+    const { threadID, messageID, body } = event;
+    switch (handleReply.type) {
+        case "tagadmin": {
+            let name = await Users.getNameUser(handleReply.author);
+            api.sendMessage({body: `====『 ADMIN TAG 』====\n◆━━━━━━━━◆◆━━━━━━━━◆\n\n💬 Message: ${body}\n\nReply T0o This Message T0o Respond`, attachment: await downLoad(`https://graph.facebook.com/${event.senderID}/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, __dirname+'/cache/12345.jpg')}, handleReply.threadID, (err, info) => {
+                if(err) console.log(err)
+                else {
+                    global.client.handleReply.push({
+                        name: this.config.name,
+                        type: "reply",
+                        messageID: info.messageID,
+                        messID: messageID,
+                        threadID
+                    })
+                }
+            }, handleReply.messID);
+            break;
+        }
+        case "reply": {
+            let name = await Users.getNameUser(event.senderID);
+            api.sendMessage({body: `===== 𝗨𝗦𝗘𝗥 𝗙𝗘𝗘𝗗𝗕𝗔𝗖𝗞 =====\n━━━━━━━━━━━━━━━━━━\n\nMessage: ${body}\nNaMe : ${name || "Facebook User"}\nGroup NaMe: ${(await Threads.getInfo(threadID)).threadName || "Unknow"}\nTiMe and DaTe: ${moment().tz("Asia/Manila").format("DD/MM/YYYY-HH:mm:ss")}\n\nReply T0o This Message T0o Continue The Conversation.`, attachment: await downLoad(`https://graph.facebook.com/${event.senderID}/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, __dirname+'/cache/12345.jpg')},handleReply.threadID, (err, info) => {
+                if(err) console.log(err)
+                else {
+                    global.client.handleReply.push({
+                        name: this.config.name,
+                        type: "tagadmin",
+                        messageID: info.messageID,
+                        messID: messageID,
+                        threadID
+                    })
+                }
+            }, handleReply.messID);
+            break;
+        }
+    }
+}
+
+module.exports.handleEvent = async ({ api, event, Users, Threads, args }) => {
+    const { threadID, messageID, body, mentions, senderID } = event;
+    let path = __dirname + "/cache/tagadmin.json";
+    if(!fs.existsSync(path)) fs.writeFileSync(path, "{}");
+    let data = JSON.parse(fs.readFileSync(path));
+    if(!data[threadID]) data[threadID] = true;
+    if(!mentions || !data[threadID]) return;
+    let mentionsKey = Object.keys(mentions);
+    let allAdmin = global.config.ADMINBOT;
+    mentionsKey.forEach(async (each) => {
+        if(each == api.getCurrentUserID()) return;
+        if(allAdmin.includes(each)) {
+            let userName = await Users.getNameUser(senderID);
+            let threadName = await Threads.getInfo(threadID).threadName;
+            api.sendMessage({body:`=====『 𝗧𝗔𝗚 𝗔𝗗𝗠𝗜𝗡 』=====\n◆━━━━━━━━◆◆━━━━━━━━◆\n👤 NaMe: ${userName}\n👨‍👩‍👧‍👦 𝗕𝗼𝘅: ${(await Threads.getInfo(threadID)).threadName || "Unknown"}\n💬 Message: ${body}\n⏰ TiMe and DaTe: ${moment().tz("Asia/Karachi").format("DD/MM/YYYY-HH:mm:ss")}\n\nReply T0o This Message T0o Continue Conversation.`, attachment: await downLoad(`https://graph.facebook.com/${event.senderID}/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, __dirname+'/cache/12345.jpg')},each, (err, info) => {
+                if(err) console.log(err)
+                else {
+                    global.client.handleReply.push({
+                        name: this.config.name,
+                        type: "tagadmin",
+                        messageID: info.messageID,
+                        messID: messageID,
+                        author: each,
+                        threadID
+                    })
+                }
+            })
+        }
+    })
+    fs.writeFileSync(path, JSON.stringify(data, null, 4));
+}
+
+module.exports.run = async ({ api, event, args }) => {
+const fs = require("fs");
+    const { threadID } = event;
+    let path = __dirname + "/cache/tagadmin.json";
+    if(!fs.existsSync(path)) fs.writeFileSync(path, "{}");
+    let data = JSON.parse(fs.readFileSync(path));
+    if(!data[threadID]) data[threadID] = true;
+    if(args[0] == "off") data[threadID] = false;
+    else if(args[0] == "on") data[threadID] = true;
+    else return api.sendMessage({body: `on or off only`, attachment: fs.createReadStream(__dirname + `/noprefix/bank.gif`)}, event.threadID, event.messageID);
+    fs.writeFileSync(path, JSON.stringify(data, null, 4));
+    return api.sendMessage({body: `Tag Admin already ${data[threadID] ? "on" : "off"}`, attachment: fs.createReadStream(__dirname + `/noprefix/bank.gif`)}, event.threadID, event.messageID);
+};
+
+async function downLoad(a, b) {
+    await (require('image-downloader')).image({
+        url: a, dest: b
+    });
+    return (require('fs-extra')).createReadStream(b);
+};
