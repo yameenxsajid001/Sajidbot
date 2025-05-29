f5cedd9a38046af80765f356adf5356afa42f6b2
Jordanxhks <jordenxhk3@gmail.com>
2025-05-30 04:45:28 +0500

    Update antiout.js

--- Priyansh/events/antiout.js
+++ Priyansh/events/antiout.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "antiout",
  eventType: ["log:unsubscribe"],
  version: "0.0.1",
- credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
+ credits: "DungUwU",
  description: "Listen events"
 };
 
@@ -11,12 +11,12 @@ module.exports.run = async({ event, api, Threads, Users }) => {
  if (data.antiout == false) return;
  if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
  const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
- const type = (event.author == event.logMessageData.leftParticipantFbId) ? "self-separation" : "Koi Ase Pichware Mai Lath Marta Hai?";
+ const type = (event.author == event.logMessageData.leftParticipantFbId) ? "self-separation" : "being kicked by the administrator";
  if (type == "self-separation") {
   api.addUserToGroup(event.logMessageData.leftParticipantFbId, event.threadID, (error, info) => {
    if (error) {
-    api.sendMessage(`Isse Dubara Add Nhi Kar Paya 🥺 ${name} Group Mai :( `, event.threadID)
-   } else api.sendMessage(`Bhag Ke Jaane Ka Nhi, ${name} Baby, Dekho Phir Se Add Kardiya Aapko`, event.threadID);
+    api.sendMessage(`😒𝗧𝗲𝗥𝗲 𝗞𝗼 𝗠𝗮𝗶𝗡𝘆 𝗔𝗗𝗗 𝗡𝗵𝗶 𝗞𝗥𝗿𝗡𝗮 𝗪𝗽𝘀 ${name} `, event.threadID)
+   } else api.sendMessage(`𝐌𝐞𝐑𝐢 𝐉𝐚𝐚𝐍 ✦ ${name} ✦ 𝐍𝐡𝐢 𝐉𝐚𝐍𝐚 𝐌𝐞𝐊𝐨 𝐂𝐡𝐨𝐫 𝐊𝐑𝐫 🥺`, event.threadID);
   })
  }
 }
