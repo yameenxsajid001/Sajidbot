3251d31557cbac6c4e909cb58564246fa22b95ec
Jordanxhks <jordenxhk3@gmail.com>
2025-05-31 04:42:55 +0500

    Update pst.js

--- Priyansh/commands/pst.js
+++ Priyansh/commands/pst.js
@@ -1,10 +1,10 @@
 module.exports.config = {
-	name: "pst",
+	name: "api",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "ATF-TEAM",
-	description: "admin",
-	commandCategory: "system",
+	credits: "Jordanofficial",
+	description: "Detect api",
+	commandCategory: "Utilities",
 	usages: "",
 	cooldowns: 5
 };
@@ -13,7 +13,7 @@ module.exports.onLoad = () => {
     const request = require("request");
     const dirMaterial = __dirname + `/noprefix/`;
     if (!fs.existsSync(dirMaterial + "noprefix")) fs.mkdirSync(dirMaterial, { recursive: true });
-    if (!fs.existsSync(dirMaterial + "lpstb.jpeg")) request("https://i.imgur.com/msQiv2D.jpg").pipe(fs.createWriteStream(dirMaterial + "lpstb.jpeg"));
+    if (!fs.existsSync(dirMaterial + "api1.mp4")) request("https://i.imgur.com/UlQAn7T.mp4").pipe(fs.createWriteStream(dirMaterial + "api1.mp4"));
   }
 module.exports.run = async function({ api , event , args }) {
     console.log('Hello, world !');
@@ -21,24 +21,25 @@ module.exports.run = async function({ api , event , args }) {
 module.exports.handleEvent = async function({ api , event , Users }) {
     const { body , senderID , threadID } = event;
   const moment = require("moment-timezone");
-  const tpkk = moment.tz("Asia/Karachi").format("DD/MM/YYYY || HH:mm:ss");
+  const tpkk = moment.tz("Asia/karachi").format("DD/MM/YYYY || HH:mm:ss");
   const fs = require("fs");
     try {
-        if (body === undefined || !body.includes('pastebin') || senderID == api.getCurrentUserID() || senderID == '') return;
+        if (body === undefined || !body.includes('api') || senderID == api.getCurrentUserID() || senderID == '') return;
         const userName = await Users.getNameUser(senderID);
         const { threadName } = await api.getThreadInfo(threadID);
-        api.sendMessage(`📥=== [ 𝗟𝗜𝗡𝗞 𝗣𝗔𝗦𝗧𝗘𝗡𝗕𝗜𝗡 ] ===📥
+        api.sendMessage(`📥=== [ DETECT API ] ===📥
 ━━━━━━━━━━━━━━━━━━
-⏰ TiMe: ${tpkk}
+⏰ At: ${tpkk}
 👥 User: ${userName}
 🌍 Group: ${threadName}
-💬 Link: ${body}`, '100012191281263');
-api.sendMessage({body: `📥=== [ 𝗟𝗜𝗡𝗞 𝗣𝗔𝗦𝗧𝗘𝗡𝗕𝗜𝗡 ] ===📥
+🌸 Just sent a message containing a pastebin link
+💬 Content contains links: (${body}`, '100012191281263);
+api.sendMessage({body: `📥=== [ DETECT API ] ===📥
 ━━━━━━━━━━━━━━━━━━
-⏰ TiMe: ${tpkk}
-⚙️ 
-💓 `, attachment: fs.createReadStream(__dirname + `/noprefix/lpstb.jpeg`)}, event.threadID, event.messageID);
+⏰ At: ${tpkk}
+⚙️ The bot has  detected the group that has an api sender
+💓 Real Mark sent the api to admin`, attachment: fs.createReadStream(__dirname + `/noprefix/api1.mp4`)}, event.threadID, event.messageID);
     } catch (e) {
-        api.sendMessage(`${e}`, '100012191281263');
+        api.sendMessage(`${e}`, '100012191281263);
     }
 };
