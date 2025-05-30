6c1b697fec7bed403421d4a4c79fb2ec0cc75fce
Jordanxhks <jordenxhk3@gmail.com>
2025-05-31 04:32:02 +0500

    Update uid.js

--- Priyansh/commands/uid.js
+++ Priyansh/commands/uid.js
@@ -1,17 +1,25 @@
-    module.exports.config = {
+module.exports.config = {
 	name: "uid",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "Mirai Team",
+	credits: "AMIR",
 	description: "Get User ID.",
 	commandCategory: "Tools",
 	cooldowns: 5
 };
 
-module.exports.run = function({ api, event }) {
-	if (Object.keys(event.mentions) == 0) return api.sendMessage(`${event.senderID}`, event.threadID, event.messageID);
-	else {
-		for (var i = 0; i < Object.keys(event.mentions).length; i++) api.sendMessage(`${Object.values(event.mentions)[i].replace('@', '')}: ${Object.keys(event.mentions)[i]}`, event.threadID);
-		return;
-	}
-}
+module.exports.run = function ({ api, event }) {
+  if (Object.keys(event.mentions).length === 0) {
+    if (event.messageReply) {
+      const senderID = event.messageReply.senderID;
+      return api.shareContact(senderID, event.messageReply.senderID, event.threadID);
+    } else {
+      return api.shareContact(`${event.senderID}`, event.senderID, event.threadID, event.messageID);
+    }
+  } else {
+    for (const mentionID in event.mentions) {
+      const mentionName = event.mentions[mentionID];
+      api.shareContact(`${mentionName.replace('@', '')}: ${mentionID}`, mentionName, event.threadID);
+    }
+  }
+};
