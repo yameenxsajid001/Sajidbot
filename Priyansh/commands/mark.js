aae55774c2f9742428aba2435f93ce838d50f040
Jordanxhks <jordenxhk3@gmail.com>
2025-05-31 01:43:58 +0500

    Update mark.js

--- Priyansh/commands/mark.js
+++ Priyansh/commands/mark.js
@@ -1,74 +1,21 @@
 module.exports.config = {
-	name: "mark",
-	version: "1.0.1",
+	name: "owner",
+	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "MewMew mod By hungdz30cm",
-	description: "Comment on the board",
-	commandCategory: "game",
-	usages: "[text]",
-	cooldowns: 5,
-	dependencies: {
-		"canvas":"",
-		 "axios":"",
-		 "fs-extra":""
-	}
-};
-
-module.exports.wrapText = (ctx, text, maxWidth) => {
-	return new Promise(resolve => {
-		if (ctx.measureText(text).width < maxWidth) return resolve([text]);
-		if (ctx.measureText('W').width > maxWidth) return resolve(null);
-		const words = text.split(' ');
-		const lines = [];
-		let line = '';
-		while (words.length > 0) {
-			let split = false;
-			while (ctx.measureText(words[0]).width >= maxWidth) {
-				const temp = words[0];
-				words[0] = temp.slice(0, -1);
-				if (split) words[1] = `${temp.slice(-1)}${words[1]}`;
-				else {
-					split = true;
-					words.splice(1, 0, temp.slice(-1));
-				}
-			}
-			if (ctx.measureText(`${line}${words[0]}`).width < maxWidth) line += `${words.shift()} `;
-			else {
-				lines.push(line.trim());
-				line = '';
-			}
-			if (words.length === 0) lines.push(line.trim());
-		}
-		return resolve(lines);
-	});
-} 
-
-module.exports.run = async function({ api, event, args }) {
-	let { senderID, threadID, messageID } = event;
-	const { loadImage, createCanvas } = require("canvas");
-	const fs = global.nodemodule["fs-extra"];
-	const axios = global.nodemodule["axios"];
-	let pathImg = __dirname + '/cache/markngu.png';
-	var text = args.join(" ");
-	if (!text) return api.sendMessage("Enter the content of the comment on the board", threadID, messageID);
-	let getPorn = (await axios.get(`https://i.imgur.com/3j4GPdy.jpg`, { responseType: 'arraybuffer' })).data;
-	fs.writeFileSync(pathImg, Buffer.from(getPorn, 'utf-8'));
-	let baseImage = await loadImage(pathImg);
-	let canvas = createCanvas(baseImage.width, baseImage.height);
-	let ctx = canvas.getContext("2d");
-	ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
-	ctx.font = "400 45px Arial";
-	ctx.fillStyle = "#000000";
-	ctx.textAlign = "start";
-	let fontSize = 45;
-	while (ctx.measureText(text).width > 2250) {
-		fontSize--;
-		ctx.font = `400 ${fontSize}px Arial, sans-serif`;
-	}
-	const lines = await this.wrapText(ctx, text, 440);
-	ctx.fillText(lines.join('\n'), 95,420);//comment
-	ctx.beginPath();
-	const imageBuffer = canvas.toBuffer();
-	fs.writeFileSync(pathImg, imageBuffer);
-return api.sendMessage({ attachment: fs.createReadStream(pathImg) }, threadID, () => fs.unlinkSync(pathImg), messageID);        
+  credits: "John Arida",
+	description: "Owner",
+	commandCategory: "Others",
+	cooldowns: 5
 }
+
+module.exports.run =  ({ api, event, args, client, Users, Threads, __GLOBAL, Currencies }) => {
+  const axios = global.nodemodule["axios"];
+  const request = global.nodemodule["request"];
+  const fs = global.nodemodule["fs-extra"];
+    var callback = () => api.sendMessage(
+  {body:`» Owner of ${global.config.BOTNAME} Bot «\n➟ ${global.config.BOTOWNER} Senpai\n❂ Admin UID: ${global.config.OWNERID}\n♛ Admin FB Link:\n${global.config.OWNERLINK}`, attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+    fs.unlinkSync(__dirname + "/cache/1.png"));  
+      return request(encodeURI(`https://graph.facebook.com/${global.config.OWNERID}/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
+fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
+    
+      };
