2d86b7ff3b4e7e66ff1257e4b799978677ad405b
Jordanxhks <jordenxhk3@gmail.com>
2025-05-31 01:56:00 +0500

    Create botname.js

---
+++ Priyansh/commands/botname.js
@@ -0,0 +1,35 @@
+module.exports.config = {
+    name: "botname",
+    version: "1.0.4",
+    hasPermssion: 1,
+    creditss: "jordanofficial",
+    description: "Automatically prevent change bot nickname",
+    commandCategory: "owner",
+    usages: "",
+    cooldowns: 5
+};
+
+
+module.exports.handleEvent = async function ({ api, args, event, client, __GLOBAL, Threads, Currencies }) {
+    const { threadID } = event;
+    let { nicknames } = await api.getThreadInfo(event.threadID)
+    const nameBot = nicknames[api.getCurrentUserID()]
+    if (nameBot !== `[ ${config.PREFIX} ] • ${config.BOTNAME}`) {
+        api.changeNickname(`[ ${global.config.PREFIX} ] • ${(!global.config.BOTNAME) ? "Made by CatalizCS and SpermLord" : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
+        setTimeout(() => {
+            return api.sendMessage(`BoT Ka NaMe ChanGe KRrny Ki Permission  OnLy Onwer ☆ JORDAN ☆ Ke PaSs Hai 🤞😎`, threadID);
+        }, 1500);
+    }
+}
+
+module.exports.run = async({ api, event, Threads}) => {
+    let data = (await Threads.getData(event.threadID)).data || {};
+    if (typeof data["cnamebot"] == "undefined" || data["cnamebot"] == false) data["cnamebot"] = true;
+    else data["cnamebot"] = false;
+    
+    await Threads.setData(event.threadID, { data });
+    global.data.threadData.set(parseInt(event.threadID), data);
+    
+    return api.sendMessage(`✅ ${(data["cnamebot"] == true) ? "Turn on" : "Turn off"} successfully cnamebot!`, event.threadID);
+
+}
