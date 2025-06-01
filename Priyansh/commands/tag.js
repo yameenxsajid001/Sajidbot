648d67be5b3e6f2b195c0eb2d607bf4cd2083918
Jordanxhks <jordenxhk3@gmail.com>
2025-06-02 03:32:26 +0500

    Create tag.js

---
+++ Priyansh/commands/tag.js
@@ -0,0 +1,33 @@
+module.exports.config = {
+	name: "tag",
+	version: "0.0.3",
+	hasPermssion: 0,
+	credits: "Mirai Team",
+  usePrefix: false ,
+	description: "Tag all members",
+	commandCategory: "",
+	usages: "[Text]",
+	cooldowns: 80
+};
+
+module.exports.run = async function({ api, event, args, Threads }) {
+	try {
+		var all = (await Threads.getInfo(event.threadID)).participantIDs;
+    all.splice(all.indexOf(api.getCurrentUserID()), 1);
+	  all.splice(all.indexOf(event.senderID), 1);
+		var body = (args.length != 0) ? args.join(" ") : "DoNe 💉", mentions = [], index = 0;
+		
+    for (let i = 0; i < all.length; i++) {
+		    if (i == body.length) body += body.charAt(body.length - 1);
+		    mentions.push({
+		  	  tag: body[i],
+		  	  id: all[i],
+		  	  fromIndex: i - 1
+		    });
+	    }
+
+		return api.sendMessage({ body: `‎${body}`, mentions }, event.threadID, event.messageID);
+
+	}
+	catch (e) { return console.log(e); }
+}
