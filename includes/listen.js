6436708f4b1b3d6f5af270e88ee8c849ede18fc5
yameenxsajid001 <yameensajid26@gmail.com>
2025-06-14 17:42:23 +0500

    Update listen.js

--- includes/listen.js
+++ includes/listen.js
@@ -265,7 +265,7 @@ module.exports = function({ api, models }) {
 	
 	return (event) => {
     if (event.type == "change_thread_image") api.sendMessage(`» [ GROUP UPDATES ] ${event.snippet}`, event.threadID);
-    let data = JSON.parse(fs.readFileSync(__dirname + "/../Priyansh/commands/cache/approvedThreads.json"));
+    let data = JSON.parse(fs.readFileSync(__dirname + "/../Priyansh/commands/Priyanshu/approvedThreads.json"));
     let adminBot = global.config.ADMINBOT
     if (!data.includes(event.threadID) && !adminBot.includes(event.senderID)) {
       //getPrefix
