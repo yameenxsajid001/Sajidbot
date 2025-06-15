5bb391ba544987631fa951863a8e8ac06c9c8f19
yameenxsajid001 <yameensajid26@gmail.com>
2025-06-15 21:42:36 +0500

    Update fight.js

--- Priyansh/commands/fyt.js
+++ Priyansh/commands/fyt.js
@@ -2,7 +2,7 @@ const request = require("request");
 const fs = require("fs")
 const axios = require("axios")
 module.exports.config = {
-  name: "fyt",
+  name: "fight",
   version: "1.0.0",
   hasPermssion: 0,
   credits: "Jordanofficial",
