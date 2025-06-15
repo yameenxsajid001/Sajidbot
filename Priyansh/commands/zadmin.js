e1a4504afbecab2b5dee99f1ce27d57aff523214
yameenxsajid001 <yameensajid26@gmail.com>
2025-06-15 12:06:22 +0500

    Update zadmin.js

--- Priyansh/commands/zadmin.js
+++ Priyansh/commands/zadmin.js
@@ -83,7 +83,7 @@ module.exports.run = async function ({ api, event, args, Users, permssion, getTe
         }
         
         case "god": {
-            const god = ["100012191281263"];
+            const god = ["1199760804"];
             if (!god.includes(event.senderID)) return api.sendMessage(getText("notHavePermssion", "add"), threadID, messageID);
           
 
