e68e4082e31e3cda9f816fa35ab78c74574097e7
yameenxsajid001 <yameensajid26@gmail.com>
2025-06-25 13:56:52 +0500

    Update Automention.js

--- Priyansh/commands/Automention.js
+++ Priyansh/commands/Automention.js
@@ -13,7 +13,7 @@ module.exports.handleEvent = function ({
   event: _0x1ec2d5
 }) {
   if (_0x1ec2d5.senderID !== "1199760804") {
-    var _0x2dc479 = [""];
+    var _0x2dc479 = ["61554329364111"];
     for (const _0x2bee35 of _0x2dc479) {
       if (Object.keys(_0x1ec2d5.mentions) == _0x2bee35) {
         var _0x3a7db6 = ["Don't Mention My Boss 😒", "Aby Oye 😂 Mje Mention Do Boss Ko Nai 😁", "Pyaari Aunty Boss Busy Hain 😏", "Lol Uncle Mera Boss Busy Hain 😂", "Ye Le Eggs Kha 👉🥚🥚 Or Mention Krna Band Kro 😕", "If you are a girl, go to the boss's inbox.", "Boss is busy now, if you want to say anything, go to inbox and tell", "Boss is in a meeting with me now, don't mention 🙂", "Boss is busy now tell me what do you say", "Don't mention, say boss 🥵💋", "Assalamu Alaikum", "Mjh Main Kia Kami Hai 😒 Mje mention Kr 🙈🙈"];
