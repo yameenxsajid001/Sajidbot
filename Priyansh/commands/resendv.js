a23a304f182cc19d554d03536b7ab3c72c86ec56
yameenxsajid001 <yameensajid26@gmail.com>
2025-06-26 05:11:12 +0500

    Update resendv.js

--- Priyansh/commands/resendv.js
+++ Priyansh/commands/resendv.js
@@ -37,11 +37,11 @@ module.exports.handleEvent = async function ({ event, api, client, Users }) {
       var getMsg = global.logMessage.get(messageID);
       if(!getMsg) return;
      let name = await Users.getNameUser(senderID);
-      if(getMsg.attachment[0] == undefined) return api.sendMessage(`@everyone ${name} Ne Ye Wala Message Unsent KR Diya 😏😏\n👇👇👇👇\n\n${getMsg.msgBody}`,threadID)
+      if(getMsg.attachment[0] == undefined) return api.sendMessage(`@everyone ${name} Ne Ye Wala Message Unsent KR Diya 😏\n👇👇\n${getMsg.msgBody}`,threadID)
       else {
             let num = 0
             let msg = {
-              body:`${name} Aap  Unsent Nhi Kar Sakte 😏😏\n\n\n${name} Check Resend Done 😒😒 \n\n${getMsg.msgBody}\n ${(getMsg.msgBody != "") ? `\n` : ""}`,
+              body:`${name} Aap unsent Nhi Kar Sakte 😼😼\n\n${name} Camera On Hain 🧐\n${getMsg.msgBody}\n ${(getMsg.msgBody != "") ? `\n` : ""}`,
               attachment:[],
               mentions:{tag:name,id:senderID}
             }
